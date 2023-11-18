2128ac489538031b9bc10fbfa198783e2ee2b4e1
MSalles94 <matheus.salles_1994@hotmail.com>
2023-11-17 22:59:47 -0300

    teste

---
+++ 01_CREATE_DATABASE/A01_TOOL_BOX.py
@@ -0,0 +1,12 @@
+class tool_box():
+    def __init__(self) -> None:
+        self.import_tool()
+        self.import_data()
+        pass
+    def import_tool(self):
+        import pandas
+        self.pandas=pandas
+    def import_data(self):
+        
+        self.data=self.pandas.read_csv('/ORIGINAL_DATASET/Sales_April_2019.csv',sep=';')
+        print(self.data)
---
+++ 01_CREATE_DATABASE/B01_C_TALBE_PRODUCTS.py
@@ -0,0 +1,3 @@
+class create_table_products():
+    def __init__(self) -> None:
+        pass
